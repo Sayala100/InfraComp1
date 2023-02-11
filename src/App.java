73fdafb8ebeb09147a261c27000391b9ada080f0
Santiago Ayala <UniandesLogin@uniandes.edu.co>
2023-02-11 13:14:49 -0500

    s

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el numero de productos: ");
+        int n = sc.nextInt();
+
+        System.out.println("Ingrese el tamaño del buffer 1: ");
+        int b1 = sc.nextInt();
+
+        System.out.println("Ingrese el tamaño del buffer 2: ");
+        int b2 = sc.nextInt();
+    }
+}
