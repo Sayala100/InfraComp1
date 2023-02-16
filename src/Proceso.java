0014c4c1cadaaa198a16109b45bd0172d71c8b67
Franisis <f.guzmana@uniandes.edu.co>
2023-02-16 09:34:27 -0500

    Monitor/Proceso

---
+++ src/Proceso.java
@@ -0,0 +1,22 @@
+public class Proceso extends Thread
+{
+    /* 
+    *Atributos
+    */
+    public static String mensajeRecibido;
+
+    public static String mensajeEnviar;
+
+    public static long id;
+
+    /*
+    * Métodos
+    */
+
+    public Proceso()
+    {
+
+    }
+    
+
+}
